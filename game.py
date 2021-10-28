24695aae4a6457deb78419c8376b5fd3c1afe691
RK912 <52097605+RK912@users.noreply.github.com>
2021-10-28 13:25:34 +0900

    Data Visualisation
    1. Retrieved probabilities of each quantum state from the state vector. 
    2. Added data visualisation (histogram + qsphere).

--- game.py
+++ game.py
@@ -1,22 +1,10 @@
 import pygame, sys, time, qiskit, math
-from qiskit import Aer, transpile
-simulator = Aer.get_backend('aer_simulator')
+from qiskit import *
+import numpy as np
+from qiskit.quantum_info import Statevector
+import matplotlib.pyplot as plt
 
-# Gate selector buttons
-class Button:
-  def __init__(self, width, height, name, color):
-    self.width = width
-    self.height = height
-    self.name = name
-    self.color = color
-   
-  # Returns true if mouse position is on the button
-  def mouseOnButton(self, left, top, mouse_pos):
-    return (mouse_pos[0] > left and mouse_pos[0] < left + self.width and mouse_pos[1] > top and mouse_pos[1] < top + self.height)
-  
-  # Render button
-  def draw(self, left, top): 
-    pygame.draw.rect(screen, self.color, pygame.Rect(left, top, self.width, self.height))
+probabilities = [] # Used to keep all probability data
 
 def quitGame():
   pygame.display.quit()
@@ -33,7 +21,6 @@ def drawGrid(boundingRect):
   for i in range(GRID_SIZE):
     pygame.draw.rect(screen, (255,0,0), (boundingRect.left, (i+1)*hori_spacing + boundingRect.top, boundingRect.width ,GRID_THICKNESS))
 
-# Convert mouse's x,y position to i,j on the grid    
 def mouseCoordToGrid(boundingRect):
   mouse_pos = pygame.mouse.get_pos()
   vert_spacing = boundingRect.width/(GRID_SIZE+1)
@@ -42,30 +29,18 @@ def mouseCoordToGrid(boundingRect):
   j = math.floor((mouse_pos[1] - boundingRect.top - 0.5 * hori_spacing)/hori_spacing)
   if i < 0 or i > GRID_SIZE - 1 or j < 0 or j > GRID_SIZE - 1:
     i = -1
-    j = -1
+    j -1
   return(i, j)
 
-# Handles user input
 def handleEvents():
-  events = pygame.event.get()
-  for i in range(len(events)):
-    if events[i].type == pygame.QUIT:
-      quitGame()
-    if events[i].type == pygame.MOUSEBUTTONDOWN:
-      # check if pressing button
-      for i in range(len(buttons)):
-        if buttons[i].mouseOnButton(bounding_box.left + i * (buttons[i].width + button_spacing), bounding_box.top + bounding_box.height + button_margin, pygame.mouse.get_pos()):
-          print("BUTTON " + buttons[i].name)
-          global current_gate # very funky python is weird
-          current_gate = buttons[i].name
-          return
-      # check if out of bounds
-      if (mouse_pos[0]) < 0:
-        return
-      if (current_gate == '0'):
-        return
-      # update board
-      board[mouse_pos[0]][mouse_pos[1]] = current_gate
+    events = pygame.event.get()
+    for i in range(len(events)):
+        if events[i].type == pygame.QUIT:
+          quitGame()
+        if events[i].type == pygame.MOUSEBUTTONDOWN:
+            if (mouse_pos[0]) < 0:
+                return
+            board[mouse_pos[0]][mouse_pos[1]] = 'H'
 
 def boardToCircuit():
   global board
@@ -92,37 +67,63 @@ def drawGridElements(boundingRect):
     for j in range(GRID_SIZE):
       # H gate
       if board[i][j] == "H":
-        pygame.draw.circle(screen, (255, 0, 0), (boundingRect.left + (i+1)*vert_spacing, boundingRect.top + (j+1)*hori_spacing), 30)
+        pygame.draw.circle(screen, (0,0,200), (boundingRect.left + (i+1)*vert_spacing, boundingRect.top + (j+1)*hori_spacing), 30)
       # X gate
       if board[i][j] == "X":
-        pygame.draw.circle(screen, (255, 255, 0), (boundingRect.left + (i+1)*vert_spacing, boundingRect.top + (j+1)*hori_spacing), 30)
-      # Y gate
-      if board[i][j] == "Y":
-        pygame.draw.circle(screen, (0, 0, 255), (boundingRect.left + (i+1)*vert_spacing, boundingRect.top + (j+1)*hori_spacing), 30)
-      # Z gate
-      if board[i][j] == "Z":
-        pygame.draw.circle(screen, (0, 255, 0), (boundingRect.left + (i+1)*vert_spacing, boundingRect.top + (j+1)*hori_spacing), 30)
+        pygame.draw.circle(screen, (0,200,0), (boundingRect.left + (i+1)*vert_spacing, boundingRect.top + (j+1)*hori_spacing), 30)
 
-def drawButtons(buttons, left, top):
-  for i in range(len(buttons)):
-    buttons[i].draw(left + i * (buttons[i].width + button_spacing), top)
-    
-def calculateScores():
-  global simulator
-  circuits = boardToCircuit()
-  circuits[0].save_statevector()
-  circuits[0] = transpile(circuits[0], simulator)
-  resultp1 = simulator.run(circuits[0]).result()
-  statevectorp1 = resultp1.get_statevector(circuits[0])
+
+
+#Deleted this function and replaced with getProbability.
+#def calculateScores():
+ # global simulator
+  #circuits = boardToCircuit()
+  #circuits[0].save_statevector()
+  #circuits[0] = transpile(circuits[0], simulator)
+  #resultp1 = simulator.run(circuits[0]).result()
+  #statevectorp1 = resultp1.get_statevector(circuits[0])
+
+  
+  
+# Calculate Probability Vector
+def getProbability(qc):
+    state = Statevector(qc)
+    probs = state.probabilities()
+    probabilities.append(probs)
+    return probs #NumPy array
+
+
+# Visualising the Probability Distribution
+
+# Generate all possible binary strings of length n
+# list returns [0, 1, 2, ..., 1<<n - 1] in binary
+def generateCode(n):
+  if n==1:
+    return ['0', '1']
+  
+  else:
+    return ['0'+x for x in generateCode(n-1)] + ['1'+x for x in generateCode(n-1)]
+
+# Plot probability using matplotlib
+# needs repair (for histogram display)
+def plotProbability(data): # data given by numpy array
+  n = math.log2(data.size)
+  xk = generateCode(n)
+  pk = data
+
+  plt.plot(np.arange(data.size), pk, 'bo')
+  plt.xticks(np.arange(data.size), xk)
+
+
+# Visualising the Quantum State as a QSphere
+def qsphere(qc):
+    state = Statevector(qc)
+    state.draw('qsphere')
 
 pygame.init()
-
-# Create screen
-SCREEN_X = 640 * 1.5
-SCREEN_Y = 480 * 1.5
+SCREEN_X = 640
+SCREEN_Y = 480
 screen = pygame.display.set_mode((SCREEN_X,SCREEN_Y))
-
-# Create grid board
 GRID_SIZE = 3
 GRID_THICKNESS = 5
 board = []
@@ -131,24 +132,10 @@ for i in range(GRID_SIZE):
   for j in range(GRID_SIZE):
     row.append("0")
   board.append(row)
-bounding_box = pygame.Rect(100,100,400,400)
-  
-# Create buttons  
-current_gate = '0'
-button_width = 50
-button_height = 50
-button_margin = 50
-button_spacing = 50
-buttons = [
-  Button(button_width, button_height, 'H', (255, 0, 0)),
-  Button(button_width, button_height, 'X', (255, 255, 0)),
-  Button(button_width, button_height, 'Y', (0, 0, 255)),
-  Button(button_width, button_height, 'Z', (0, 255, 0)),
-]
-  
+
 while True:
+    bounding_box = pygame.Rect(50,100,400,400)
     drawGrid(bounding_box)
-    drawButtons(buttons, bounding_box.left, bounding_box.top + bounding_box.height + button_margin)
     mouse_pos = mouseCoordToGrid(bounding_box)
     drawGridElements(bounding_box)
     handleEvents()
